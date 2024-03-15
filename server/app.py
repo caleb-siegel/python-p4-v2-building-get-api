a0528854ed7b828522ce984f36eee1863c558e57
Caleb Siegel <caleb.siegel@gmail.com>
2024-03-15 10:51:43 -0400

    completed lab

--- server/app.py
+++ server/app.py
@@ -21,6 +21,55 @@ def index():
 
 # start building your API here
 
+@app.route('/games')
+def games():
+
+    # games = []
+    # for game in Game.query.all():
+    #     game_dict = game.to_dict()
+    #     games.append(game_dict)
+
+    games = [game.to_dict() for game in Game.query.all()]
+
+
+    response = make_response(
+        games,
+        200,
+        {"Content-Type": "application/json"}
+    )
+
+    return response
+
+@app.route('/games/<int:id>')
+def game_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+
+    game_dict = game.to_dict()
+
+    response = make_response(
+        game_dict,
+        200
+    )
+
+    return response
+
+@app.route('/games/users/<int:id>')
+def game_users_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+    # users = []
+    # for review in game.reviews:
+    #     user = review.user
+    #     user_dict = user.to_dict(rules=("-reviews",))
+    #     users.append(user_dict)
+    users = [review.user.to_dict(rules=('-reviews',))
+            for review in game.reviews]
+
+    response = make_response(
+        users,
+        200
+    )
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
